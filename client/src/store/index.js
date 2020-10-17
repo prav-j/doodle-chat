7efcbbd0e2679908dd6d19775c7e7223fcf69be9
Praveen Janakarajan <praveen.41128@gmail.com>
2020-10-17 23:44:22 +0200

    Add socket middleware to store

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,6 +1,13 @@
-import {createStore} from 'redux'
+import {applyMiddleware, compose, createStore} from 'redux'
 import reducers from './reducers';
+import socketMiddleware from '../middlewares/socket'
 
 export default () => {
-  return createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+  const middlewares = [socketMiddleware()]
+  return createStore(reducers,
+    compose(
+      applyMiddleware(...middlewares),
+      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+  )
 }
