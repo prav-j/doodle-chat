1c053465f0cfecee34e273a618b1d1dd78665188
Praveen Janakarajan <praveen.41128@gmail.com>
2020-10-17 21:21:33 +0200

    Create barebones Ktor server

---
+++ server/src/Application.kt
@@ -0,0 +1,84 @@
+package com.praveen.doodle
+
+import com.fasterxml.jackson.databind.SerializationFeature
+import io.ktor.application.*
+import io.ktor.auth.*
+import io.ktor.client.*
+import io.ktor.client.features.auth.*
+import io.ktor.client.features.json.*
+import io.ktor.features.*
+import io.ktor.http.*
+import io.ktor.http.cio.websocket.*
+import io.ktor.jackson.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import io.ktor.websocket.*
+import kotlinx.coroutines.runBlocking
+import java.time.Duration
+
+fun main() {
+    embeddedServer(
+        Netty,
+        watchPaths = listOf("server"),
+        port = 8080,
+        module = Application::module
+    ).apply { start(wait = true) }
+}
+
+fun Application.module() {
+    install(WebSockets) {
+        pingPeriod = Duration.ofSeconds(15)
+        timeout = Duration.ofSeconds(15)
+        maxFrameSize = Long.MAX_VALUE
+        masking = false
+    }
+
+    install(Authentication) {
+    }
+
+    install(ContentNegotiation) {
+        jackson {
+            enable(SerializationFeature.INDENT_OUTPUT)
+        }
+    }
+
+    HttpClient {
+        install(Auth) {
+        }
+        install(JsonFeature) {
+            serializer = GsonSerializer()
+        }
+    }
+    runBlocking {
+        // Sample for making a HTTP Client request
+        /*
+        val message = client.post<JsonSampleClass> {
+            url("http://127.0.0.1:8080/path/to/endpoint")
+            contentType(ContentType.Application.Json)
+            body = JsonSampleClass(hello = "world")
+        }
+        */
+    }
+
+    routing {
+        get("/") {
+            call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
+        }
+
+        webSocket("/chat/echo") {
+            send(Frame.Text("Hi from server"))
+            while (true) {
+                val frame = incoming.receive()
+                if (frame is Frame.Text) {
+                    send(Frame.Text("Client said: " + frame.readText()))
+                }
+            }
+        }
+
+        get("/json/jackson") {
+            call.respond(mapOf("hello" to "world"))
+        }
+    }
+}
