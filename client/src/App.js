e21f21246a437a2b13911147555fd06800e8ac6d
Praveen Janakarajan <praveen.41128@gmail.com>
2020-10-18 21:59:29 +0200

    Add login screen

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import {hot} from 'react-hot-loader';
-import Messaging from "./components/Messaging";
+import Login from "./screens/Login";
+import Container from "@material-ui/core/Container";
+import Paper from "@material-ui/core/Paper";
+import {useTheme} from "@material-ui/core";
 
-const App = () =>
-  <div className="App">
-    <Messaging/>
-  </div>
+const styles = {
+  root: (theme) => ({
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: theme.palette.primary.light,
+    height: '100vh'
+  }),
+}
+
+const App = () => {
+  const theme = useTheme()
+  return <div style={styles.root(theme)}>
+    <Container maxWidth="sm">
+      <Paper>
+        <Login/>
+      </Paper>
+    </Container>
+  </div>;
+}
 
 export default hot(module)(App);
---
+++ client/src/screens/Login.js
@@ -0,0 +1,64 @@
+import React, {useState} from "react";
+import {TextField} from "@material-ui/core";
+import {Alert} from "@material-ui/lab";
+import Button from "@material-ui/core/Button";
+import Box from "@material-ui/core/Box";
+import {useDispatch, useSelector} from "react-redux";
+import {signupUser} from "../store/user/actions";
+
+const styles = {
+  container: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'column',
+    height: '75vh'
+  },
+  field: {
+    width: '50%'
+  },
+  hidden: {
+    opacity: 0
+  },
+  usernameField: {marginBottom: '4em'},
+  passwordField: {marginBottom: '2em'},
+  errorField: {marginBottom: '1em'},
+  horizontalGroup: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    width: '50%'
+  }
+}
+
+export default () => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const error = useSelector(state => state.user.error)
+  const dispatch = useDispatch()
+  return <>
+    <Box style={styles.container}>
+      <TextField
+        style={{...styles.field, ...styles.usernameField}}
+        label="Username"
+        variant="outlined"
+        helperText="What should others call you?"
+        value={username}
+        onChange={(event) => setUsername(event.target.value)}/>
+      <TextField
+        style={{...styles.field, ...styles.passwordField}}
+        label="Your password"
+        variant="outlined"
+        helperText="Enter your password, or pick one if you're new"
+        value={password}
+        onChange={(event) => setPassword(event.target.value)}/>
+      <Alert style={{...styles.errorField, ...(error ? {} : styles.hidden)}} severity="error">
+        {error}
+      </Alert>
+      <Box style={styles.horizontalGroup}>
+        <Button variant="contained" onClick={() => dispatch(signupUser(username, password))}>Sign up</Button>
+        <Button variant="contained">Login</Button>
+      </Box>
+    </Box>
+  </>
+}
