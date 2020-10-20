69d9981f7b1f63e059b146fa61e0f1d3cd1ba666
Praveen Janakarajan <praveen.41128@gmail.com>
2020-10-20 22:53:41 +0200

    Store message times as unix timestamps

--- server/resources/db/migration/V2__create-messages-table.sql
+++ server/resources/db/migration/V2__create-messages-table.sql
@@ -2,6 +2,6 @@ create TABLE MESSAGES (
     id uuid primary key,
     content text NOT NULL,
     sent_by varchar(100) NOT NULL,
-    sent_at timestamp default now(),
+    sent_at bigint default extract(epoch from now()) * 1000,
     FOREIGN KEY(sent_by) REFERENCES USERS(name)
 );
--- server/src/message/Message.kt
+++ server/src/message/Message.kt
@@ -2,18 +2,15 @@ package com.praveen.doodle.message
 
 import com.praveen.doodle.user.Users
 import org.jetbrains.exposed.dao.id.UUIDTable
-import org.jetbrains.exposed.sql.`java-time`.datetime
-import java.time.LocalDateTime
-import java.util.*
 
 data class Message(
     val content: String,
     val sentBy: String,
-    val sentAt: Date = Date()
+    val sentAt: Long = System.currentTimeMillis()
 )
 
 object Messages : UUIDTable("messages") {
     val content = text("content")
     val user = varchar("sent_by", 100).references(Users.username)
-    val sentAt = datetime("sent_at").default(LocalDateTime.now())
+    val sentAt = long("sent_at").default(System.currentTimeMillis())
 }
