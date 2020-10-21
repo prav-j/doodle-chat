a756123d76fbba0d6972f25385f758917bcb869e
Praveen Janakarajan <praveen.41128@gmail.com>
2020-10-22 01:03:52 +0200

    Add Dockerfile for server

--- server/src/database/Config.kt
+++ server/src/database/Config.kt
@@ -4,19 +4,22 @@ import com.typesafe.config.ConfigFactory
 
 data class DbConnectionParams(
     val url: String,
-    val name: String,
     val user: String,
-    val password: String
+    val password: String,
+    val database: String
 )
 
 fun getDatabaseConnectionInfo(): DbConnectionParams {
     val config = ConfigFactory.load()
     val dbConfig = config.getConfig("database")
 
+    val user = dbConfig.getString("user")
+    val password = dbConfig.getString("password")
+    val database = dbConfig.getString("database")
     return DbConnectionParams(
-        dbConfig.getString("url"),
-        dbConfig.getString("name"),
-        dbConfig.getString("user"),
-        dbConfig.getString("password")
+        "jdbc:postgresql://${System.getenv("DB_HOST")}/$database",
+        user,
+        password,
+        database
     )
 }
